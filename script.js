11d811c79e5238f7d2bf409c197a9d3ebf3984c0
oluwafisayo.takuro <oluwafisayo.takuro@interswitchng.com>
2023-04-19 15:04:27 +0100

    Made changes to the UI and added a Project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //TYPEWRITER EFFECT
-const intro = "Hello, I am Oluwafisayo Takuro. I am a frontend developer.";
+const intro = "Hello, I am Oluwafisayo Takuro. I am a Frontend Developer.";
 const speed = 90;
 
 let i = 0;
@@ -24,4 +24,15 @@ window.addEventListener("scroll", () => {
   const nav = document.querySelector("nav");
   let windowPosition = window.scrollY > 1100 && window.scrollY < 2200;
   nav.classList.toggle("scroll-active", windowPosition);
+});
+
+//CHANGE ACTIVE CLASS
+let navbarLink = document.querySelectorAll(".navbar-nav .nav-link");
+
+document.addEventListener("click", (e) => {
+  for (let i = 0; i < navbarLink.length; i++) {
+    navbarLink[i].classList.remove("active");
+  }
+
+  e.target.closest(".nav-link").classList.add("active");
 });
